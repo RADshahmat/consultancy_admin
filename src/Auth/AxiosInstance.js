8e5c482c17e0adb077adf6c91a96f61dc3666e42
HR khan Ratin <134430653+baten005@users.noreply.github.com>
2024-06-26 16:53:50 +0600

    Update AxiosInstance.js

--- src/Auth/AxiosInstance.js
+++ src/Auth/AxiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3002',
+  baseURL: 'https://backend-doc-8n06.onrender.com',
   withCredentials: true, // Ensure this option is set
 });
 
